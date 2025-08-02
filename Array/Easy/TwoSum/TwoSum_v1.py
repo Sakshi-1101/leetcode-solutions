397b79c58737c1ffd2d059a8c7b87b67cf4ee2f8
Sakshi Gupta <sakshigupta11101@gmail.com>
2025-08-02 17:15:18 +0530

    Add TwoSum_v1.py

---
+++ Array/Easy/TwoSum/TwoSum_v1.py
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        // System.out.println("testing github push");
+        // System.out.println("testing versioning");
+
+        for(int i = 0 ; i < nums.length ; i ++){
+            if(map.containsKey(nums[i])){
+                ans[0] = map.get(nums[i]);
+                ans[1] = i ;
+                break;
+            }
+            else{
+                int remSum = target - nums[i];
+                map.put(remSum, i);
+            }
+        }
+
+        return ans;
+
+    }
+}
