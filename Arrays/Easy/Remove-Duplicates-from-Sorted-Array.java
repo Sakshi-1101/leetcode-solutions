e384552800f53ac23e076e5e2791951efe14fcf2
Sakshi Gupta <sakshigupta11101@gmail.com>
2025-08-30 00:48:39 +0530

    [26] [Time Beats: 59.63%] [Memory Beats: 91.55%] - LeetPush

---
+++ Arrays/Easy/Remove-Duplicates-from-Sorted-Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] arr) {
+         int i = 0;
+        
+        // we move ‘j’ till we don't get a number arr[j] which is different from arr[i]. 
+        //As we got a unique number we will increase the i pointer and update its value by arr[j]
+        for(int j = 1 ; j < arr.length ; j ++){
+
+            if(arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+
+        return i + 1; // i + 1 to return total unique elements
+        
+    }
+}
