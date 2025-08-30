032f6380a3b87ed298d73f85111d08101e79f7cf
Sakshi-1101 <sakshigupta11101@gmail.com>
2025-08-31 02:02:27 +0530

    added comments in leetcode ques

--- Arrays/Easy/Missing-Number.java
+++ Arrays/Easy/Missing-Number.java
@@ -1,4 +1,12 @@
 class Solution {
+
+    // TC: O(N)
+    // SC: O(1)
+    /*
+     * Approach: 
+     * In this approach, we calculate the expected sum of numbers from 0 to n using the formula n*(n+1)/2. 
+     * We then subtract the actual sum of the elements in the array from this expected sum to find the missing number.
+     */
     public int missingNumber(int[] nums) {
         int n = nums.length;
         int totalSum = n * (n + 1) / 2;
