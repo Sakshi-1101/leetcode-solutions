d65d27ac3296c74a7b8a6347cd164b7308f8c0dd
Sakshi Gupta <sakshigupta11101@gmail.com>
2025-09-14 00:23:47 +0530

    [121] [Time Beats: 72.24%] [Memory Beats: 75.34%] - LeetPush

---
+++ Arrays/Medium/Best-Time-to-Buy-and-Sell-Stock.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length <= 1) {
+            return 0;
+        }
+        
+        int min = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1 ; i < prices.length ; i ++) {
+            int profit = prices[i] - min;
+            maxProfit = Math.max(maxProfit, profit);
+            min = Math.min(min, prices[i]);
+        }
+
+        return maxProfit;
+
+        
+    }
+}
