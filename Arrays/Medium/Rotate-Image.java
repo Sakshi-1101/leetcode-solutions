a605d4fcb2bfa63fe815a5e9be48b7825c52c7fc
Sakshi Gupta <sakshigupta11101@gmail.com>
2025-09-27 00:06:52 +0530

    [48] [Time Beats: 100.00%] [Memory Beats: 78.44%] - LeetPush

---
+++ Arrays/Medium/Rotate-Image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        //transpose the matrix
+        for(int i = 0 ; i < matrix.length ; i ++) {
+            for(int j = i ; j < matrix[0].length ; j ++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // reverse each row of the transposed matrix
+        for(int i = 0 ; i < matrix.length ; i ++) {
+            for(int j = 0 ; j < matrix[0].length / 2 ; j ++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = temp;
+            }
+        }
+    }
+}
